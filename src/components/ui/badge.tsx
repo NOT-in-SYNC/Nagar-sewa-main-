a0cfb5cf88bc02e1c8729664d529b1e14ba1fdef
NOT-in-SYNC <02122005naitik@gmail.com>
2025-09-03 20:14:20 +0530

    badge.tsx

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+  "inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 tracking-tight",
   {
     variants: {
       variant: {
@@ -15,6 +15,9 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        info: "bg-[#2A327A] text-white border-transparent",
+        success: "bg-[#13851B]/10 text-[#13851B] border-[#13851B]/30",
+        notice: "bg-[#FF9700]/10 text-[#FF9700] border-[#FF9700]/30",
       },
     },
     defaultVariants: {
